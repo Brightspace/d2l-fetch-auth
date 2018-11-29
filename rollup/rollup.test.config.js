0c30cfde761878047e14c4f02fc498e097d450ad
Anthony Wikkerink <Anthony.Wikkerink@D2L.com>
2018-11-29 13:13:26 -0500

    Build base rollup for pretest

--- rollup/rollup.test.config.js
+++ rollup/rollup.test.config.js
@@ -1,6 +1,7 @@
 import { config } from './rollup.common.config.js';
+import baseConfig from './rollup.config.js';
 
-export default [
+export default baseConfig.concat([
 	config('d2lfetch', './src/unframed/d2lfetch-auth.js', './test/dist/d2lfetch-auth.internals.js'),
 	config('d2lfetch', './src/framed/d2lfetch-auth-framed.js', './test/dist/d2lfetch-auth.framedInternals.js')
-];
+]);
