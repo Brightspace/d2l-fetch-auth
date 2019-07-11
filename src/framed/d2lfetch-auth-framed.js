aab5b510b0a34200c7e648bf27984dcdbcab44c6
Owen Smith <owen@omsmith.ca>
2019-07-11 00:38:30 -0400

    Omsmith/remove trust (#24)
    * Revert "check url trust before attaching tokens (#22)"
    
    This reverts commit e49adcce1a6713f0238f6a468d44a33fbf2ceffb.
    
    * deps: restore good changes to lockfile
    
    [increment patch]

--- src/framed/d2lfetch-auth-framed.js
+++ src/framed/d2lfetch-auth-framed.js
@@ -1,7 +1,4 @@
-import initializeUrlTrust from 'd2l-fetch-auth-trust/framed';
 import { default as jwt } from 'frau-jwt/framed';
-
-const urlTrust = initializeUrlTrust();
 
 export class D2LFetchAuthFramed {
 
@@ -13,10 +10,6 @@ export class D2LFetchAuthFramed {
 		next = next || function(r) { return Promise.resolve(r); };
 
 		if (request.headers.get('Authorization')) {
-			return next(request);
-		}
-
-		if (!urlTrust.shouldTrust(request.url)) {
 			return next(request);
 		}
 
