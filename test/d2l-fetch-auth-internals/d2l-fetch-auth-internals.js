8885c12075e98f2b1e6e5f7142ce770967c3b770
Owen Smith <owen@omsmith.ca>
2019-07-14 19:32:21 -0400

    Revert "local: call D2L.LP token-getter (#23)"
    This reverts commit 75b3efe23e1d7c9f99025624086f95f09244b377.
    
    In some cases the called code was as of yet unavailable.
    
    [increment patch]

--- test/d2l-fetch-auth-internals/d2l-fetch-auth-internals.js
+++ test/d2l-fetch-auth-internals/d2l-fetch-auth-internals.js
@@ -1,14 +1,208 @@
 import { D2LFetchAuth } from '../dist/d2lfetch-auth.internals.js';
 
 describe('D2LFetchAuth class internals', function() {
-	var d2lFetchAuth;
+	var d2lFetchAuth,
+		xsrfTokenKey = 'XSRF.Token',
+		xsrfTokenValue = 'foo',
+		defaultScope = '*:*:*',
+		authToken = {
+			access_token: 'such access wow',
+			expires_at: Number.MAX_VALUE
+		},
+		authTokenResponse = {
+			headers: { 'x-csrf-token': xsrfTokenValue },
+			body: { access_token: authToken.access_token, expires_at: authToken.expires_at }
+		};
+
+	function clock() {
+		return (Date.now() / 1000) | 0;
+	}
 
 	beforeEach(function() {
+		setXsrfToken(xsrfTokenValue);
+		sinon.stub(window, 'fetch');
 		d2lFetchAuth = new D2LFetchAuth();
+	});
+
+	afterEach(function() {
+		clearXsrfToken();
+		window.fetch.restore();
 	});
 
 	it('should be a thing', function() {
 		expect(d2lFetchAuth).to.be.an.instanceOf(D2LFetchAuth);
+	});
+
+	function clearXsrfToken() {
+		window.localStorage.removeItem(xsrfTokenKey);
+	}
+
+	function setXsrfToken(value) {
+		window.localStorage.setItem(xsrfTokenKey, value);
+	}
+
+	describe('XSRF request', function() {
+		it('should send a XSRF request when the XSRF token does not exist in local storage', function() {
+			clearXsrfToken();
+
+			window.fetch.returns(
+				Promise.resolve(
+					new Response(
+						'{ "referrerToken": "' + xsrfTokenValue + '" }'
+					)
+				)
+			);
+
+			return d2lFetchAuth._getXsrfToken()
+				.then(function(xsrfToken) {
+					expect(xsrfToken).to.equal(xsrfTokenValue);
+				});
+		});
+
+		it('should use xsrf token if it exists in local storage', function() {
+			setXsrfToken('oh yeah, awesome');
+
+			return d2lFetchAuth._getXsrfToken()
+				.then(function(xsrfToken) {
+					expect(xsrfToken).to.equal('oh yeah, awesome');
+				});
+		});
+	});
+
+	describe('Auth token request', function() {
+		afterEach(function() {
+			d2lFetchAuth._resetAuthTokenCaches();
+		});
+
+		it('should send an auth token request when auth token does not exist', function(done) {
+			window.fetch.returns(
+				Promise.resolve(
+					new Response(
+						JSON.stringify(authTokenResponse.body),
+						{ status: 200, referrerToken: xsrfTokenValue }
+					)
+				)
+			);
+
+			d2lFetchAuth._getAuthToken()
+				.then(function(authToken) {
+
+					var req = window.fetch.args[0][0];
+					expect(req.method).to.equal('POST');
+					expect(req.url).to.have.string('/d2l/lp/auth/oauth2/token');
+					expect(req.headers.get('x-csrf-token')).to.equal(xsrfTokenValue);
+					expect(authToken).to.equal(authTokenResponse.body.access_token);
+					req.text().then(function(bodyText) {
+						expect(bodyText).to.equal('scope=' + defaultScope);
+						done();
+					});
+				});
+		});
+
+		it('should send an auth token request when auth token is expired', function(done) {
+			window.fetch.returns(
+				Promise.resolve(
+					new Response(
+						JSON.stringify(authTokenResponse.body),
+						{ status: 200, referrerToken: xsrfTokenValue }
+					)
+				)
+			);
+
+			d2lFetchAuth._cacheToken(defaultScope, {
+				access_token: 'token',
+				expires_at: clock() - 1
+			});
+
+			d2lFetchAuth._getAuthToken()
+				.then(function(authToken) {
+
+					var req = window.fetch.args[0][0];
+					expect(req.method).to.equal('POST');
+					expect(req.url).to.have.string('/d2l/lp/auth/oauth2/token');
+					expect(req.headers.get('x-csrf-token')).to.equal(xsrfTokenValue);
+					expect(authToken).to.equal(authTokenResponse.body.access_token);
+					req.text().then(function(bodyText) {
+						expect(bodyText).to.equal('scope=' + defaultScope);
+						done();
+					});
+				});
+		});
+
+		it('should account for server clock skew when deciding if a token is expired', function() {
+			// put the server ~10 minutes ahead
+			const skew = 10 * 60;
+			setupResponse();
+
+			// call once to get clock skew calculation
+			return d2lFetchAuth
+				._getAuthToken()
+				.then(() => {
+					// overwrite the, what would be now cached token
+					// the expiry value of this token wouldn't normally be considered expired
+					// but the skew should push it over
+					d2lFetchAuth._cacheToken(defaultScope, {
+						access_token: 'token',
+						expires_at: clock() + Math.round(skew / 2)
+					});
+
+					setupResponse();
+					return d2lFetchAuth
+						._getAuthToken()
+						.then(authToken => {
+							expect(window.fetch.args[0][0].url).to.have.string('/d2l/lp/auth/oauth2/token');
+							expect(authToken).to.equal(authTokenResponse.body.access_token);
+						});
+				});
+
+			function setupResponse() {
+				const serverDateString = new Date((clock() + skew) * 1000).toUTCString();
+				window.fetch.returns(
+					Promise.resolve(
+						new Response(
+							JSON.stringify(authTokenResponse.body),
+							{ status: 200, headers: { Date: serverDateString } }
+						)
+					)
+				);
+			}
+		});
+
+		it('should use cached auth token if it exists', function() {
+			d2lFetchAuth._cacheToken(defaultScope, authToken);
+			return d2lFetchAuth._getAuthToken()
+				.then(function(token) {
+					expect(token).to.equal(authToken.access_token);
+				});
+		});
+
+		it('should not use cached tokens after session change', function(done) {
+			window.fetch.returns(
+				Promise.resolve(
+					new Response(
+						JSON.stringify(authTokenResponse.body),
+						{ status: 200, referrerToken: xsrfTokenValue }
+					)
+				)
+			);
+
+			var alternativeToken = {
+				access_token: 'cool beans',
+				expires_at: Number.MAX_VALUE
+			};
+
+			d2lFetchAuth._cacheToken(defaultScope, alternativeToken);
+			d2lFetchAuth._getAuthToken()
+				.then(function(token) {
+					expect(token).to.equal(alternativeToken.access_token);
+					d2lFetchAuth._onStorage({ key: 'Session.UserId' });
+					d2lFetchAuth._getAuthToken()
+						.then(function(token) {
+							expect(token).to.equal(authToken.access_token);
+							done();
+						});
+				});
+		});
 	});
 
 	describe('isRelativeUrl', function() {
